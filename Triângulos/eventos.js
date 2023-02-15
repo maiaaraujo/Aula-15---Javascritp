3837eccaa86bb789c6c02711d3b498c610b4b1ec
LucasDCoelho <jessicamaiaaraujo@gmail.com>
2023-02-15 09:43:28 -0300

    triangulos

--- Triângulos/eventos.js
+++ Triângulos/eventos.js
@@ -1,11 +1,14 @@
-let ladoA = prompt("Qual o lado A?")
-let ladoB = prompt("Qual o lado B?")
-let ladoC = prompt("Qual o lado C?")
+let ladoA = prompt("Qual o lado A?");
+let ladoB = prompt("Qual o lado B?");
+let ladoC = prompt("Qual o lado C?");
 
 
 
-if (ladoA == ladoB == ladoC){
-    alert("Triângulo Equilátero");
-} else {
-    alert("Não é equilatero");
+if (ladoA == ladoB && ladoB == ladoC){
+    alert("Equilátero");
+}  else if (ladoA != ladoB && ladoB != ladoC && ladoC != ladoA) {
+    alert("escaleno");
+}   else {
+    alert("isósceles");
 }
+

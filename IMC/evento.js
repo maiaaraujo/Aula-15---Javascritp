7d945c9dc2acdccb5f95b8baafe8cc25624fb87b
LucasDCoelho <jessicamaiaaraujo@gmail.com>
2023-02-15 11:35:24 -0300

    imc funções

--- IMC/evento.js
+++ IMC/evento.js
@@ -3,5 +3,20 @@ function calcularIMC(){
     let mPeso = Number(peso.value);
     let mAltura = Number(altura.value);
     let imc = (mPeso / (mAltura * mAltura)).toFixed(1);
+
+    if(imc <= 18.5){
+        alert("Abaixo do peso"+ imc);
+    } else if(imc > 18.5 && imc <= 24.9){
+        alert("Peso médio"+ imc);
+    } else if(imc > 24.9 && imc <= 29.9){
+        alert("Acima do peso"+ imc);
+    } else if(imc > 29.9 && imc <= 34.9){
+        alert("Obesidade 1:"+ imc);
+    } else if(imc > 34.9 && imc <= 39.9){
+        alert("Obesidade 2:"+ imc)
+    } else{
+        alert("Obesidade 3:"+ imc);
+    } 
+      
     alert(imc);
 }
